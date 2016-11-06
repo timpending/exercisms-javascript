14c0669e657d81738df9e3a3765f49525f321adf
Tim Pender <mrtimpender@gmail.com>
2016-11-06 16:23:46 -0700

    submitted ex2 js

---
+++ leap/leap.js
@@ -0,0 +1,9 @@
+function Year(year) {
+  this.year = year
+}
+
+Year.prototype.isLeap = function() {
+  return this.year%400=== 0 || (this.year%4 === 0 && this.year%100 !== 0)
+}
+
+module.exports = Year
