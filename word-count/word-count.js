bb5b191591b44287bc81ab60b208fd3617695979
Tim Pender <mrtimpender@gmail.com>
2016-11-20 11:28:04 -0700

    word count complete

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -4,7 +4,6 @@ function Words(input){
   this.count = function(input) {
     let wordArray = input.match(/\S+/g)
     let wordCloud = {};
-    let count = 1;
     for (let i=0; i<wordArray.length; i++){
         let lcWord = wordArray[i].toLowerCase();
         wordCloud[lcWord] = wordCloud.hasOwnProperty(lcWord) ? wordCloud[lcWord] + 1 : 1
