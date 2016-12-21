235206d57ec4604955907e5208f415350644fff5
Tim Pender <mrtimpender@gmail.com>
2016-12-21 14:58:57 -0500

    all individual verses completed for food-chain

--- food-chain/food-chain.js
+++ food-chain/food-chain.js
@@ -1,30 +1,90 @@
-function FoodChain() {
-  this.animalArray = ['fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow'];
-};
+function FoodChain() {};
 
 FoodChain.prototype.verse = function(x, y){
-  let song = ''
+  let song = '';
+  if (x >= 8 ){
+    song = 'I know an old lady who swallowed a horse.\n' + 'She\'s dead, of course!\n'
+    return song;
+  }
   if (!y) {
-    switch (x) {
-      case 2:
-      console.log('in case 2');
-      this.firstLine(x);
-      break;
-    default:
-      song += this.firstLine(x);
-      break;
+    if (x > 1) {
+      song += this.firstLine(this.getAnimal(x));
+      song += this.secondLine(this.getAnimal(x));
+      song += this.swallowLines(x);
+    }
+    if (x===1){
+      song += this.firstLine(this.getAnimal(x));
     }
     song += `I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n`;
   }
   return song;
 }
+
+FoodChain.prototype.getAnimal = function (position) {
+  let animalArray = ['fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow'];
+  return animalArray[position-1];
+};
+
+FoodChain.prototype.firstLine = function(animal){
+  return `I know an old lady who swallowed a ${animal}.\n`;
+}
+
+FoodChain.prototype.secondLine = function (animal){
+  switch (animal) {
+    case 'spider':
+      return `It wriggled and jiggled and tickled inside her.\n`
+      break;
+    case 'bird':
+      return `How absurd to swallow a bird!\n`
+      break;
+    case 'cat':
+      return 'Imagine that, to swallow a cat!\n'
+      break;
+    case 'dog':
+      return 'What a hog, to swallow a dog!\n'
+      break;
+    case 'goat':
+      return 'Just opened her throat and swallowed a goat!\n'
+      break;
+    case 'cow':
+      return 'I don\'t know how she swallowed a cow!\n'
+      break;
+    default:
+      break;
+  }
+};
+
+FoodChain.prototype.swallowLines = function (position){
+  let animalArray = ['fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow'];
+  let lines = '';
+  while (position > 1){
+      if (position === 3) {
+        lines += `She swallowed the ${animalArray[position-1]} to catch the ${animalArray[position-2]} that wriggled and jiggled and tickled inside her.\n`
+        position--;
+      }
+      lines += `She swallowed the ${animalArray[position-1]} to catch the ${animalArray[position-2]}.\n`;
+      position--;
+    }
+  return lines;
+};
+
+
+
+
+//
+// I know an old lady who swallowed a fly.
+// I don't know why she swallowed the fly. Perhaps she'll die.
+//
+//√ I know an old lady who swallowed a spider.
+// It wriggled and jiggled and tickled inside her.
+// She swallowed the spider to catch the fly.
+// √ I don't know why she swallowed the fly. Perhaps she'll die.
+//
 // 'I know an old lady who swallowed a bird.\n' +
 //   'How absurd to swallow a bird!\n' +
 //   'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' +
 //   'She swallowed the spider to catch the fly.\n' +
 //   'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
-
-
 
 // Build the fly outcome (last line).
 // Opener for the animal.
@@ -32,15 +92,5 @@ FoodChain.prototype.verse = function(x, y){
 // if Fly ==> i know... [fly]... i dont know why, perhaps she'll die
 // Return array position plus 1, reference that, and then return the specific instance.
 
-FoodChain.prototype.firstLine = function(position){
-  let animal = this.animalArray[position - 1]
-  return `I know an old lady who swallowed a ${animal}.\n`;
-}
-
-
-// FoodChain.prototype.first(animal){
-//
-//
-// }
 
 module.exports = FoodChain;
